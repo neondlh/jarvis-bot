226a8242f1af3d6cba2283f2a057e00d752b214e
Fernanda De la Hoz <maria.fernanda.delahoz@gmail.com>
2017-06-21 13:44:51 -0400

    no message

--- src/main/java/com/jarvis/bot/JarvisController.java
+++ src/main/java/com/jarvis/bot/JarvisController.java
@@ -9,8 +9,15 @@ import org.alicebot.ab.Bot;
 import org.alicebot.ab.Chat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +35,7 @@ public class JarvisController {
 	private static final Logger logger = LoggerFactory.getLogger(JarvisController.class);
 	private static final String verifyToken = "xyz123";
 	private static final String pageAccessToken = "EAADmeR5hib8BAC5Dc8lpaXjnEHFKuAyLnjVJZAjH8ctFElQVO96xn9GSMDJsqyV0AJ09js0VBEmjLMBdORHEpQd3eTmjTiRTAECuLnZCqNcq2ZBtfTp8a1veHbOYkzqxyYM1JmZBEr8cdSmLDa1O0C5WIpzDK9QxNcNtili8jQZDZD";
-	private final String requestUrl = String.format("https://graph.facebook.com/v2.8/me/messages?access_token=%s", new Object[] { pageAccessToken  });
+	private final String requestUrl = "https://graph.facebook.com/v2.6/me/messages?access_token=EAADmeR5hib8BAC5Dc8lpaXjnEHFKuAyLnjVJZAjH8ctFElQVO96xn9GSMDJsqyV0AJ09js0VBEmjLMBdORHEpQd3eTmjTiRTAECuLnZCqNcq2ZBtfTp8a1veHbOYkzqxyYM1JmZBEr8cdSmLDa1O0C5WIpzDK9QxNcNtili8jQZDZD";
 	private String sendId;
 	private String question;
 	private final String RESPONSE_FORMAT = "{\"recipient\": {\"id\": \"%s\"},\"message\": {\"text\": \"%s\"}}";
@@ -106,8 +113,18 @@ public class JarvisController {
         	String response = "Test response";
         	
         	RestTemplate template = new RestTemplate();
-        	String result = template.postForObject(requestUrl, String.format(RESPONSE_FORMAT, sendId, response), String.class);
-            logger.error("Result: " + result);
+        	String request = String.format(RESPONSE_FORMAT, sendId, response);
+        	logger.error("Request: {}", request);
+        	
+        	HttpHeaders headers = new HttpHeaders();
+        	headers.setContentType(MediaType.APPLICATION_JSON);
+        	HttpEntity<String> requestEntity = new HttpEntity<String>(request, headers);
+
+        	
+			ResponseEntity<String> result = template.exchange(requestUrl, HttpMethod.POST, requestEntity,String.class);
+			
+            logger.error("Result: " + result.getBody());
+            
             return ResponseEntity.status(HttpStatus.OK).build();
         } catch (Exception e) {
             logger.warn("Processing of callback payload failed: {}", e.getMessage());
